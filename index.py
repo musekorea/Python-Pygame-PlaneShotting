51f1eddc05e2367ebb3ef3f80c29a060189a0deb
musekorea <musekorea@hanmail.net>
2022-02-17 01:34:09 +0800

    obj class

--- index.py
+++ index.py
@@ -9,29 +9,50 @@ title = "Plane Shooting"
 pygame.display.set_caption(title)
 # 3. 게임 내 필요한 설정
 clock = pygame.time.Clock()
-plane = pygame.image.load("./plane2.png").convert_alpha()
-plane = pygame.transform.scale(plane, (80,80))
-plane_size_x, plane_size_y = plane.get_size()
-plane_x = round(size[0]/2-plane_size_x/2)
-plane_y = round(size[1]-plane_size_y-15)
-
 black = (0,0,0)
 white = (255,255,255)
+
+class Game_obj:
+  def __init(self):
+    self.x = 0 #좌표
+    self.y = 0
+
+  def put_img(self, path):
+    if path[-3:]=="png":
+      self.img = pygame.image.load(path).convert_alpha()
+    else:
+      self.img = pygame.image.load(path)
+    self.size_x, self.size_y = self.img.get_size()
+
+  def change_size(self, new_size_x, new_size_y):
+    self.img = pygame.transform.scale(self.img, (new_size_x,new_size_y))
+    self.size_x, self.size_y = self.img.get_size()
+
+  def show(self):
+    screen.blit(self.img, (self.x, self.y))
+
+plane = Game_obj()
+plane.put_img("./images/plane1.png")
+plane.change_size(70,70)
+plane.x = round(size[0]/2-plane.size_x/2)
+plane.y = round(size[1]-plane.size_y-15)
+
+
 # 4. 메인 이벤트 
-switch = 0
-while switch==0:
+shut_down = 0
+while shut_down==0:
   # 4-1. FPS 설정 
   clock.tick(60)
     # 4-2. 각종 입력 감지 
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
-      switch = 1
+      shut_down = 1
   # 4-3. 입력, 시간에 따른 변화 
   # 4-4. 그리기
   screen.fill(black)
-  screen.blit(plane, (plane_x,plane_y))
+  plane.show()
   
   # 4-5. 렌더링 
   pygame.display.flip()
 # 5. 게임 종료 
-pygame.QUIT()
+pygame.quit()
