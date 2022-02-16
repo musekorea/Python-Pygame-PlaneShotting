cda8599d72b7eacd9856cfba4062edad1c2a0c77
musekorea <musekorea@hanmail.net>
2022-02-16 20:12:45 +0800

    add plane

--- index.py
+++ index.py
@@ -9,26 +9,28 @@ title = "Plane Shooting"
 pygame.display.set_caption(title)
 # 3. 게임 내 필요한 설정
 clock = pygame.time.Clock()
+plane = pygame.image.load("./plane2.png").convert_alpha()
+plane = pygame.transform.scale(plane, (80,80))
+plane_size_x, plane_size_y = plane.get_size()
+plane_x = round(size[0]/2-plane_size_x/2)
+plane_y = round(size[1]-plane_size_y-15)
+
 black = (0,0,0)
 white = (255,255,255)
-test = 0
 # 4. 메인 이벤트 
-SB = 0
-while SB==0:
+switch = 0
+while switch==0:
   # 4-1. FPS 설정 
-  clock.tick(2)
+  clock.tick(60)
     # 4-2. 각종 입력 감지 
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
-      SB = 1
+      switch = 1
   # 4-3. 입력, 시간에 따른 변화 
-  test += 1
-  if test%2==0:
-    color = black
-  else:
-    color = white
   # 4-4. 그리기
-  screen.fill(color)
+  screen.fill(black)
+  screen.blit(plane, (plane_x,plane_y))
+  
   # 4-5. 렌더링 
   pygame.display.flip()
 # 5. 게임 종료 
