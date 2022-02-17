f81411e03b323600394d40d1460b4208de0d749f
musekorea <musekorea@hanmail.net>
2022-02-17 23:25:38 +0800

    plane move

--- index.py
+++ index.py
@@ -7,27 +7,26 @@ size = [400,800]
 screen = pygame.display.set_mode(size)
 title = "Plane Shooting"
 pygame.display.set_caption(title)
+
 # 3. 게임 내 필요한 설정
 clock = pygame.time.Clock()
 black = (0,0,0)
 white = (255,255,255)
 
 class Game_obj:
-  def __init(self):
+  def __init__(self):
     self.x = 0 #좌표
     self.y = 0
-
+    self.move = 0
   def put_img(self, path):
     if path[-3:]=="png":
       self.img = pygame.image.load(path).convert_alpha()
     else:
       self.img = pygame.image.load(path)
     self.size_x, self.size_y = self.img.get_size()
-
   def change_size(self, new_size_x, new_size_y):
     self.img = pygame.transform.scale(self.img, (new_size_x,new_size_y))
     self.size_x, self.size_y = self.img.get_size()
-
   def show(self):
     screen.blit(self.img, (self.x, self.y))
 
@@ -36,6 +35,9 @@ plane.put_img("./images/plane1.png")
 plane.change_size(70,70)
 plane.x = round(size[0]/2-plane.size_x/2)
 plane.y = round(size[1]-plane.size_y-15)
+plane.move = 5
+move_right=False
+move_left=False
 
 
 # 4. 메인 이벤트 
@@ -47,7 +49,27 @@ while shut_down==0:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       shut_down = 1
+    if event.type == pygame.KEYDOWN: 
+      if event.key == pygame.K_RIGHT: #1073741903
+        move_right=True
+      elif event.key ==1073741904: #1073741904
+        move_left=True
+    if event.type == pygame.KEYUP:
+      if event.key == pygame.K_RIGHT:
+        move_right = False
+      elif event.key ==pygame.K_LEFT:
+        move_left = False
+  
+    
   # 4-3. 입력, 시간에 따른 변화 
+  if move_right==True:
+    plane.x =plane.x+plane.move
+    if plane.x>size[0]-plane.size_x:
+      plane.x = size[0]-plane.size_x
+  elif move_left==True:
+    plane.x = plane.x-plane.move
+    if plane.x<0:
+      plane.x = 0
   # 4-4. 그리기
   screen.fill(black)
   plane.show()
